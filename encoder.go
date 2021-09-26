61786d85d5a6e8d48e283c9b0157340db46dc59a
Elon Natovich <elon.nat@gmail.com>
2021-09-27 00:47:52 +0300

    parse to shards, no headers

--- encoder.go
+++ encoder.go
@@ -1,6 +1,3 @@
-//go:build ignore
-// +build ignore
-
 // Copyright 2015, Klaus Post, see LICENSE for details.
 //
 // Simple encoder example
@@ -39,16 +36,154 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"math"
 	"os"
 	"path/filepath"
+	"sync"
 
 	"github.com/klauspost/reedsolomon"
+	log "github.com/sirupsen/logrus"
 )
 
-var dataShards = flag.Int("data", 4, "Number of shards to split the data into, must be below 257.")
-var parShards = flag.Int("par", 2, "Number of parity shards")
+const (
+	dataShards       = 10
+	parShards        = 3
+	totalShards      = dataShards + parShards
+	shardHeaderSize  = 26
+	shardSize        = 1300
+	maxShardDataSize = shardSize - shardHeaderSize
+	totalChunkBuffer = totalShards * shardSize
+	typeData         = 0xf1
+	typeParity       = 0xf2
+)
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	// log.SetFormatter(&log.JSONFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.DebugLevel)
+}
+
+var inputFile = flag.String("f", "", "Input file")
 var outDir = flag.String("out", "", "Alternative output directory")
+
+var maxChunkSize = (int64(shardSize) - shardHeaderSize) * int64(dataShards)
+
+type chunk struct {
+	bufsize         int64
+	offset          int64
+	numDataShards   uint8
+	numParityShards uint8
+	file_id         uint64
+	bytesUsed       uint16
+	totalFileSize   uint64
+	shards          [][]byte
+	chunkBuffer     [totalChunkBuffer]byte
+}
+
+func get_shards(c chunk) {
+	//
+}
+
+func Min(x, y int64) int64 {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func get_chunks(filename string, chunk_size int64) ([]chunk, error) {
+	log.Debug("Opening ", filename)
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	fileinfo, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	filesize := fileinfo.Size()
+	log.Debug("File size of ", filename, " is ", filesize)
+	log.Debug("Chunk size is ", chunk_size)
+	// Number of go routines we need to spawn.
+	concurrency := int(math.Ceil(float64(filesize) / float64(chunk_size)))
+	log.Debug("Concurrency is ", concurrency)
+	chunks := make([]chunk, concurrency)
+
+	for i := 0; i < concurrency; i++ {
+		chunks[i].bufsize = chunk_size
+		chunks[i].offset = chunk_size * int64(i)
+	}
+	// last one is the remainder
+	chunks[concurrency-1].bufsize = filesize - chunks[concurrency-1].offset
+
+	// Make channels to pass fatal errors in WaitGroup
+	fatalErrors := make(chan error)
+	wgDone := make(chan bool)
+
+	var wg sync.WaitGroup
+	wg.Add(concurrency)
+
+	log.Debug("Starting chunk goroutines")
+	for i := 0; i < concurrency; i++ {
+		// Each of these go routines will fill shards for a chunk
+		go func(chunksizes []chunk, i int) {
+			defer wg.Done()
+			log.Debug("Inside chunk goroutine number ", i)
+			this_chunk := &chunksizes[i]
+
+			//				 |------DATA SHARDS-----------| |----PARITY SHARDS-----|
+			// chunkBuffer = data_from_file + zero_padding + space_for_parity_shards
+			//				 |---bufsize---|
+
+			data_from_file := this_chunk.chunkBuffer[:this_chunk.bufsize]
+			bytesRead, err := file.ReadAt(data_from_file, this_chunk.offset)
+
+			log.Debug("Read ", bytesRead, " / ", this_chunk.bufsize, " bytes for chunk ", i)
+
+			if int64(bytesRead) != this_chunk.bufsize {
+				fatalErrors <- fmt.Errorf("get_chunks: chunk %d at offset %d read %d bytes (expected %d)",
+					i, this_chunk.offset, bytesRead, this_chunk.bufsize)
+			}
+			if err != nil {
+				fatalErrors <- err
+			}
+
+			// "shards" are slices from chunkBuffer
+			this_chunk.shards = make([][]byte, totalShards)
+			for j := 0; j < totalShards; j++ {
+				idx_start := shardSize * j
+				idx_end := shardSize * (j + 1)
+				this_chunk.shards[j] = this_chunk.chunkBuffer[idx_start:idx_end]
+			}
+		}(chunks, i)
+	}
+
+	// Important final goroutine to wait until WaitGroup is done
+	go func() {
+		wg.Wait()
+		close(wgDone)
+	}()
+
+	// Wait until either WaitGroup is done or an error is received through the channel
+	select {
+	case <-wgDone:
+		// carry on
+		break
+	case err := <-fatalErrors:
+		wg.Wait()
+		return nil, err
+	}
+	return chunks, nil
+}
 
 func init() {
 	flag.Usage = func() {
@@ -59,55 +194,93 @@ func init() {
 	}
 }
 
+func (c *chunk) getId(filename string) {
+
+}
+
 func main() {
 	// Parse command line parameters.
 	flag.Parse()
-	args := flag.Args()
-	if len(args) != 1 {
+
+	if *inputFile == "" {
 		fmt.Fprintf(os.Stderr, "Error: No input filename given\n")
 		flag.Usage()
 		os.Exit(1)
 	}
-	if (*dataShards + *parShards) > 256 {
-		fmt.Fprintf(os.Stderr, "Error: sum of data and parity shards cannot exceed 256\n")
-		os.Exit(1)
+	log.Debug("OK, file is ", *inputFile)
+
+	chunks, err := get_chunks(*inputFile, maxChunkSize)
+	checkErr(err)
+
+	out_base := filepath.Join(*outDir, filepath.Base(*inputFile))
+	log.Debugf("OUTPUT is in: %s.%04d", out_base, 1)
+
+	// Make channels to pass fatal errors in WaitGroup
+	fatalErrors := make(chan error)
+	wgDone := make(chan bool)
+
+	var wg sync.WaitGroup
+	wg.Add(len(chunks))
+
+	log.Debug("Starting encode goroutines")
+	for i, c := range chunks {
+		go func(i int, c chunk) {
+			log.Debug("Inside encode goroutine number ", i)
+			// Create encoding matrix.
+			enc, err := reedsolomon.New(dataShards, parShards)
+			if err != nil {
+				fatalErrors <- err
+			}
+
+			err = enc.Encode(c.shards)
+			if err != nil {
+				fatalErrors <- err
+			}
+
+			for j := 0; j < totalShards; j++ {
+				f, err := os.OpenFile(fmt.Sprintf("%s.%04d.%04d", out_base, i, j),
+					os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0644)
+				if err != nil {
+					fatalErrors <- err
+					return
+				}
+				f.WriteString(fmt.Sprintf("|---HEADER of %04d shard of %04d chunk---|\n", j, i))
+				if err != nil {
+					fatalErrors <- err
+				}
+
+				f.Write(c.shards[j])
+				if err != nil {
+					fatalErrors <- err
+				}
+				f.Close()
+			}
+
+			log.Debug("Finished encode goroutine number ", i)
+			wg.Done()
+		}(i, c)
 	}
-	fname := args[0]
+	// Important final goroutine to wait until WaitGroup is done
+	go func() {
+		wg.Wait()
+		close(wgDone)
+	}()
 
-	// Create encoding matrix.
-	enc, err := reedsolomon.New(*dataShards, *parShards)
-	checkErr(err)
-
-	fmt.Println("Opening", fname)
-	b, err := ioutil.ReadFile(fname)
-	checkErr(err)
-
-	// Split the file into equally sized shards.
-	shards, err := enc.Split(b)
-	checkErr(err)
-	fmt.Printf("File split into %d data+parity shards with %d bytes/shard.\n", len(shards), len(shards[0]))
-
-	// Encode parity
-	err = enc.Encode(shards)
-	checkErr(err)
-
-	// Write out the resulting files.
-	dir, file := filepath.Split(fname)
-	if *outDir != "" {
-		dir = *outDir
-	}
-	for i, shard := range shards {
-		outfn := fmt.Sprintf("%s.%d", file, i)
-
-		fmt.Println("Writing to", outfn)
-		err = ioutil.WriteFile(filepath.Join(dir, outfn), shard, 0644)
+	// Wait until either WaitGroup is done or an error is received through the channel
+	select {
+	case <-wgDone:
+		// carry on
+		break
+	case err := <-fatalErrors:
+		wg.Wait()
 		checkErr(err)
 	}
+	log.Debug("Finished")
 }
 
 func checkErr(err error) {
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %s", err.Error())
+		log.Error(err)
 		os.Exit(2)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module github.com/ElonN/arson
 go 1.17
 
 require (
+	github.com/klauspost/reedsolomon v1.9.13
+	github.com/sirupsen/logrus v1.8.1
+)
+
+require (
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
-	github.com/klauspost/reedsolomon v1.9.13 // indirect
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
 )
